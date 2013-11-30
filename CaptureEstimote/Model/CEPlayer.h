5318baf788e6021f85008dd8cd4e15484433a822
Blazej Marcinkiewicz <blazej.marcinkiewicz@polidea.com>
2013-11-30 14:40:55 +0100

    Player fix

--- CaptureEstimote/Model/CEPlayer.h
+++ CaptureEstimote/Model/CEPlayer.h
@@ -1,9 +1,15 @@
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    CEPlayerBlue = 1,
+    CEPlayerRed = 0x10000;
+} CEPlayerTeam;
 
 @interface CEPlayer : NSObject
 @property (nonatomic) int playerId;
 @property (nonatomic, copy) NSString *peerId;
+
++ (CEPlayer *)playerWithTeamId:(CEPlayerTeam)teamId;
 
 - (BOOL)isPlayerSameTeam:(CEPlayer *)player;
 @end
--- CaptureEstimote/Model/CEPlayer.m
+++ CaptureEstimote/Model/CEPlayer.m
@@ -4,13 +4,24 @@
 @implementation CEPlayer {
 
 }
+
++ (CEPlayer *)playerWithTeamId:(CEPlayerTeam)teamId {
+    CEPlayer *player = [[CEPlayer alloc] init];
+    if (teamId == CEPlayerBlue) {
+        player.playerId = 0;
+    } else {
+        player.playerId = 0x10;
+    }
+    return player;
+}
+
 - (BOOL)isPlayerSameTeam:(CEPlayer *)player {
     int otherPlayerId = player.playerId;
     int otherTeamMask;
-    if (self.playerId <= 0xFFFF) {
-        otherTeamMask = 0xFFFF0000;
+    if (self.playerId <= 0xF) {
+        otherTeamMask = 0xF0;
     } else {
-        otherTeamMask = 0xFFFF;
+        otherTeamMask = 0x0F;
     }
 
     return (otherPlayerId & otherTeamMask) == 0;
